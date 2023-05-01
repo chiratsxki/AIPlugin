7c96f8c39e6b83cfb95a34d2734a9274b68fc36e
chiratsxki <maximusabidjanov@gmail.com>
2023-05-02 02:14:00 +0500

    add a version 0.1-BETA

---
+++ src/main/java/com/chiratsxki/projectleyla/SearchAction.java
@@ -0,0 +1,42 @@
+package com.chiratsxki.projectleyla;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.lang.Language;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+public class SearchAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        Optional<PsiFile> psiFile = Optional.ofNullable(e.getData(LangDataKeys.PSI_FILE));
+        String languageTag = psiFile
+                .map(PsiFile::getLanguage)
+                .map(Language::getDisplayName)
+                .map(String::toLowerCase)
+                .map(lang -> "[" + lang + "]")
+                .orElse("");
+
+
+        Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        CaretModel caretModel = editor.getCaretModel();
+        String selectedText = caretModel.getCurrentCaret().getSelectedText();
+
+        BrowserUtil.browse("https://www.perplexity.ai/search?q=" + selectedText);
+    }
+
+    @Override
+    public void update(AnActionEvent e) {
+        Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        CaretModel caretModel = editor.getCaretModel();
+        e.getPresentation().setEnabledAndVisible(caretModel.getCurrentCaret().hasSelection());
+    }
+}
